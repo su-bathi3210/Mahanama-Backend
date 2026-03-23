cb6a6f246e0c7950cfce37a797d162f5d1aa5547
su-bathi3210 <ksuba3210@gmail.com>
2026-03-23 12:26:40 +0530

    Configured backend to accept frontend URL

--- MahanamaPaint-Backend/src/main/java/MahanamaPaintCentre/PVT/LTD/MahanamaPaint/Config/SecurityConfig.java
+++ MahanamaPaint-Backend/src/main/java/MahanamaPaintCentre/PVT/LTD/MahanamaPaint/Config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://mahanama-frontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
